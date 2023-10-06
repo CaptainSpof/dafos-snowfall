f96a0d77ace709793ff3570afe94b02aa783d226
CaptainSpof <captain.spof@gmail.com>
2023-10-06 18:29:58 +0200

    - fix(dafoltop): drop disko config
    I failed to be clever, once again. I'll try again laterer.
    
    - feat(dafoltop): activate workstation archetype
    now that it is install, the real fun begins

--- modules/home/tools/git/default.nix
+++ modules/home/tools/git/default.nix
@@ -37,5 +37,6 @@ in
         };
       };
     };
+    programs.gh.enable = true;
   };
 }
--- systems/x86_64-linux/dafoltop/default.nix
+++ systems/x86_64-linux/dafoltop/default.nix
@@ -5,23 +5,18 @@ with lib.dafos;
 {
   imports = [
     ./hardware.nix
-    ./disko.nix
   ];
 
   boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
 
   dafos = {
-    # archetypes = {
-    #   workstation = enabled;
-    # };
+    archetypes = {
+      workstation = enabled;
+    };
 
     suites = {
-      common-slim = enabled;
       desktop = enabled;
       office = enabled;
-    };
-    apps = {
-      telegram = enabled;
     };
   };
 
@@ -31,5 +26,5 @@ with lib.dafos;
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.05"; # Did you read the comment?
+  system.stateVersion = "23.11"; # Did you read the comment?
 }
