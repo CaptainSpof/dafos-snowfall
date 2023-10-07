514886e3c5d7d4e0429656332905fb0fd50d729b
CaptainSpof <captain.spof@gmail.com>
2023-10-07 23:27:45 +0200

    feat(fish): init plugin

--- modules/home/cli-apps/fish/default.nix
+++ modules/home/cli-apps/fish/default.nix
@@ -20,6 +20,12 @@ in
           starship init fish | source
         '';
 
+        interactiveShellInit = ''
+        set fzf_history_opts "--bind=ctrl-r:toggle-sort,ctrl-z:ignore"
+        set -a fzf_history_opts "--nth=4.."
+        bind \cr _fzf_search_history # HACK: override CTRL+R binding to the one defined in fzf.fish plugin
+      '';
+
         functions = {
           fish_greeting = ''
             ${pkgs.toilet}/bin/toilet -f future --gay "Dafos"
