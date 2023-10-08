1a6d3bb66c5550213cce7635db28702b5ca0f2ac
CaptainSpof <captain.spof@gmail.com>
2023-10-08 22:10:28 +0200

    feat: add channel nixpkgs-stable & nixpgs-master

--- overlays/linux/default.nix
+++ overlays/linux/default.nix
@@ -1,9 +1,9 @@
-{ ... }:
+{ channels, ... }:
 
 _final: prev:
 
 {
-  # inherit (channels.unstable) linuxPackages_latest;
+  inherit (channels.nixpkgs-master) linuxPackages_latest;
 
   # Fixes an issue with building Raspberry Pi kernels:
   # https://github.com/NixOS/nixpkgs/issues/154163
