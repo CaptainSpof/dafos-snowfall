9ae61cc0c97068e3e0a5cdfd51f4e3060b439782
CaptainSpof <captain.spof@gmail.com>
2023-10-09 13:04:07 +0200

    feat(nushell): add basic config & completion
    chunk of the config is stolen from K900

--- modules/home/cli-apps/zoxide/default.nix
+++ modules/home/cli-apps/zoxide/default.nix
@@ -4,6 +4,8 @@ with lib;
 with lib.dafos;
 let
   cfg = config.dafos.cli-apps.zoxide;
+  # FIXME: nushell doesn't exist in home modules
+  nushell.enable = true;
   fish = config.dafos.cli-apps.fish;
   zsh = config.dafos.cli-apps.zsh;
 in
@@ -16,6 +18,7 @@ in
     programs.zoxide = {
       enable = true;
       enableFishIntegration = fish.enable;
+      enableNushellIntegration = nushell.enable;
       enableZshIntegration = zsh.enable;
       options = [ "--cmd=c" ];
     };
