5e1e06a3f59694d99e1af37ef902b739571f8fd4
CaptainSpof <captain.spof@gmail.com>
2023-10-09 14:19:55 +0200

    refactor(nushell): add home module
    finally, having a home module is a bit less awkward.

---
+++ modules/home/cli-apps/nushell/default.nix
@@ -0,0 +1,21 @@
+{ lib, config, ... }:
+
+let
+  inherit (lib) mkEnableOption mkIf;
+
+  cfg = config.dafos.cli-apps.nushell;
+in
+{
+  options.dafos.cli-apps.nushell = {
+    enable = mkEnableOption "Nushell";
+  };
+
+  config = mkIf cfg.enable {
+    programs = {
+      nushell = {
+        enable = true;
+        configFile.source = ./config.nu;
+      };
+    };
+  };
+}
--- modules/home/cli-apps/zoxide/default.nix
+++ modules/home/cli-apps/zoxide/default.nix
@@ -4,8 +4,7 @@ with lib;
 with lib.dafos;
 let
   cfg = config.dafos.cli-apps.zoxide;
-  # FIXME: nushell doesn't exist in home modules
-  nushell.enable = true;
+  nushell = config.dafos.cli-apps.nushell;
   fish = config.dafos.cli-apps.fish;
   zsh = config.dafos.cli-apps.zsh;
 in
