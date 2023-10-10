329dc8b0eb2c11cf979f59398fd9fc7e9eee5e39
CaptainSpof <captain.spof@gmail.com>
2023-10-10 18:20:06 +0200

    tweak: add additional locale settings

--- modules/vars.nix
+++ modules/vars.nix
@@ -12,7 +12,9 @@ in
     email = mkOpt types.str "dafonseca.cedric@gmail.com" "The email to configure git with.";
 
     timezone = mkOpt types.str "Europe/Paris" "The timezone to use for the system.";
-    locale = mkOpt types.str "en_US.UTF-8" "The locale to use for the system.";
+    locale.default = mkOpt types.str "en_US.UTF-8" "The default locale to use for the system.";
+    locale.alt =  mkOpt types.str "fr_FR.UTF-8" "The alternative locale to use for the system.";
+
 
     shell = mkOpt types.package pkgs.fish "The shell to use for the system.";
     theme.dark = mkOpt types.str "Everforest Dark Soft" "Theme to use for the system.";
