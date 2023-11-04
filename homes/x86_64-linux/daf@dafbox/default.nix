2cd97078f891994690360205b93549c00f6ef816
CaptainSpof <captain.spof@gmail.com>
2023-11-04 15:40:53 +0100

    - feat(dafbox): add `homes/x86_64-linux/daf@dafbox`
    - fix(dafbox): remove luks device

---
+++ homes/x86_64-linux/daf@dafbox/default.nix
@@ -0,0 +1,36 @@
+{ lib, config, ... }:
+
+let
+  inherit (lib.dafos) enabled;
+in
+{
+  dafos = {
+    user = {
+      enable = true;
+      name = config.snowfallorg.user.name;
+    };
+
+    apps = {
+      alacritty = enabled;
+      emacs = enabled;
+    };
+
+    cli-apps = {
+      fish = enabled;
+      helix = enabled;
+      home-manager = enabled;
+      neovim = enabled;
+      nushell = enabled;
+      starship = enabled;
+      zellij = enabled;
+      zoxide = enabled;
+      zsh = enabled;
+      lazygit = enabled;
+    };
+
+    tools = {
+      git = enabled;
+      direnv = enabled;
+    };
+  };
+}
