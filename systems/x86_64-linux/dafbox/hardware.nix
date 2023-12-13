1656934b805e60851151d1f59682eab14a7f09c4
CaptainSpof <captain.spof@gmail.com>
2023-12-13 11:35:51 +0100

    feat(dafbox): enable wake on lan

--- systems/x86_64-linux/dafbox/hardware.nix
+++ systems/x86_64-linux/dafbox/hardware.nix
@@ -52,6 +52,7 @@ in {
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
+  networking.interfaces.eno1.wakeOnLan.enable = true;
 
   services.fwupd.enable = true;
 
