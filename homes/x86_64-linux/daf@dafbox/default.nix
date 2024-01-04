a83076c7ba4b0d01318fb035673672e8c4d0d6b7
CaptainSpof <captain.spof@gmail.com>
2024-01-04 12:54:47 +0100

    feat(firefox): revamp module

--- homes/x86_64-linux/daf@dafbox/default.nix
+++ homes/x86_64-linux/daf@dafbox/default.nix
@@ -13,6 +13,19 @@ in
     apps = {
       alacritty = enabled;
       emacs = enabled;
+      firefox = {
+        enable = true;
+        gpuAcceleration = true;
+        hardwareDecoding = true;
+        settings = {
+          # "dom.ipc.processCount.webIsolated" = 9;
+          # "dom.maxHardwareConcurrency" = 16;
+          "media.av1.enabled" = false;
+          # "media.ffvpx.enabled" = false;
+          # "media.hardware-video-decoding.force-enabled" = true;
+          "media.hardwaremediakeys.enabled" = true;
+        };
+      };
     };
 
     cli-apps = {
