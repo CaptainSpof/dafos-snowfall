53f6ce6a57e63e2eb7bcc1a10860907ef3c9ff58
CaptainSpof <captain.spof@gmail.com>
2024-04-17 13:12:08 +0200

    feat(dafoltop): remove deprecated plasma options

--- systems/x86_64-linux/dafoltop/default.nix
+++ systems/x86_64-linux/dafoltop/default.nix
@@ -60,12 +60,12 @@ in
       programs.plasma = {
         configFile = {
           # disable screensaver
-          "kscreenlockerrc"."Daemon"."Autolock" = false;
-          "kscreenlockerrc"."Daemon"."LockOnResume" = false;
+          # "kscreenlockerrc"."Daemon"."Autolock" = false;
+          # "kscreenlockerrc"."Daemon"."LockOnResume" = false;
           # touchpad settings
-          "kcminputrc"."Libinput.1267.12419.ETD2303:00 04F3:3083 Touchpad"."NaturalScroll" = true;
-          "kcminputrc"."Libinput.1267.12419.ETD2303:00 04F3:3083 Touchpad"."PointerAccelerationProfile" = 1;
-          "kcminputrc"."Libinput.1267.12419.ETD2303:00 04F3:3083 Touchpad"."TapToClick" = true;
+          # "kcminputrc"."Libinput.1267.12419.ETD2303:00 04F3:3083 Touchpad"."NaturalScroll" = true;
+          # "kcminputrc"."Libinput.1267.12419.ETD2303:00 04F3:3083 Touchpad"."PointerAccelerationProfile" = 1;
+          # "kcminputrc"."Libinput.1267.12419.ETD2303:00 04F3:3083 Touchpad"."TapToClick" = true;
         };
       };
     };
