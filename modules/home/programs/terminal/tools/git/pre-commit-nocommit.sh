49d6c80e26057f3104b09ce97cb1cc9b4179e4b2
CaptainSpof <captain.spof@gmail.com>
2024-09-09 20:58:45 +0200

    feat(git): add pre-commit-hooks

---
+++ modules/home/programs/terminal/tools/git/pre-commit-nocommit.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+#
+# To prevent debug code from being accidentally committed, simply add a comment near your
+# debug code containing the keyword NOCOMMIT and this script will abort the commit.
+#
+if git commit -v --dry-run | grep 'NOCOMMIT' >/dev/null 2>&1; then
+	echo "Trying to commit non-committable code."
+	echo "Remove the NOCOMMIT string and try again."
+	exit 1
+else
+	# Run local pre-commit hook if exists
+	if [ -e ./.git/hooks/pre-commit ]; then
+		./.git/hooks/pre-commit "$@"
+	else
+		exit 0
+	fi
+fi
